95ef5302acbf026536894f80412e5b751278c5e7
klozz <shaclone707@gmail.com>
2019-02-06 06:26:17 -0500

    XPerience: update postboot and add HMP configs since i droped EAS support in 3.18 kernel
    Change-Id: Iae8fbde17a959b6d3bca6836a3b5e8ad24a2b3d3
    Signed-off-by: klozz <shaclone707@gmail.com>

--- common.mk
+++ common.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += qcril.db
 
+# XPerience postboot based on qcom file
+PRODUCT_PACKAGES +=
+    init.xperience.postboot.sh
+
 ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)
 ifneq ($(TARGET_HAS_LOW_RAM), true)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
