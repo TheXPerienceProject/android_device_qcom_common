e220681be087dd0fff35e65b38cababcab22bc0f
klozz <klozz@vivaldi.net>
2019-09-19 08:51:20 +0000

    common: disable proprietary telephony_product_privapp-permissions-qti
    Change-Id: I76236647ec41e187f1ce318c46e2b421920a40d9
    Signed-off-by: klozz <klozz@vivaldi.net>

--- common.mk
+++ common.mk
@@ -53,10 +53,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 ifneq ($(TARGET_BOARD_AUTO),true)
+ifneq ($(QCPATH),)
 #copy telephony app's permissions
 PRODUCT_COPY_FILES += $(QCPATH)/commonsys/telephony-build/build/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
 
 PRODUCT_COPY_FILES += $(QCPATH)/commonsys/telephony-build/build/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
+endif
 endif
 
 # Permission for Wi-Fi passpoint support
