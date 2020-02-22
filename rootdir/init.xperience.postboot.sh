150d63cb936c5a40389c260292b76d5d41760edf
klozz <klozz@vivaldi.net>
2020-02-22 23:17:08 +0000

    xpe postboot:add swap option configurable for 1Gb ram devices
    added here for those devices which not have qcom post boot script
    I forget the original developer info I will search it and I'll add lather :)
    Signed-off-by: klozz <klozz@vivaldi.net>
    
    Change-Id: I1b31f45ba7d1aa85961ab0cac7a832a4d7102c2f

--- rootdir/init.xperience.postboot.sh
+++ rootdir/init.xperience.postboot.sh
@@ -632,6 +632,29 @@ function enable_memory_features() {
 
 }
 
+#swap only for 1GB devices
+function enable_swap() {
+    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+    MemTotal=${MemTotalStr:16:8}
+
+    SWAP_ENABLE_THRESHOLD=1048576
+    swap_enable=`getprop ro.vendor.xperience.config.swap`
+
+    # Enable swap initially only for 1 GB targets
+    if [ "$MemTotal" -le "$SWAP_ENABLE_THRESHOLD" ] && [ "$swap_enable" == "true" ]; then
+        # Static swiftness
+        echo 1 > /proc/sys/vm/swap_ratio_enable
+        echo 70 > /proc/sys/vm/swap_ratio
+
+        # Swap disk - 200MB size
+        if [ ! -f /data/vendor/swap/swapfile ]; then
+            dd if=/dev/zero of=/data/vendor/swap/swapfile bs=1m count=200
+        fi
+        mkswap /data/vendor/swap/swapfile
+        swapon /data/vendor/swap/swapfile -p 32758
+    fi
+}
+
 # Check panel_name
 panel_model=$(cat /sys/class/graphics/fb0/msm_fb_panel_info | grep panel_name)
 #default_color=`getprop vendor.display.enable_default_color_mode`
