bd875ffda3fa26ff17caf65e75100632e71745bd
klozz <klozz@thexperienceproject.org>
2021-10-22 01:29:56 +0200

    disable some stuff
    Signed-off-by: klozz <klozz@thexperienceproject.org>
    Change-Id: Ice0c23c4cf7584d476885b833883df7edc5f1d5c

--- common.mk
+++ common.mk
@@ -18,10 +18,10 @@ else
     $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 endif
 
-PRODUCT_BRAND := qcom
-PRODUCT_AAPT_CONFIG += hdpi mdpi
+#PRODUCT_BRAND := qcom
+#PRODUCT_AAPT_CONFIG += hdpi mdpi
 
-PRODUCT_MANUFACTURER := QUALCOMM
+#PRODUCT_MANUFACTURER := QUALCOMM
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
@@ -33,13 +33,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.vendor.shutdown.waittime=500 \
     persist.vendor.radio.procedure_bytes=SKIP
 
-ifneq ($(ENABLE_HYP),true)
-ifneq ($(BOARD_FRP_PARTITION_NAME),)
-    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)
-else
-    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/config
-endif
-endif
+#ifneq ($(ENABLE_HYP),true)
+#ifneq ($(BOARD_FRP_PARTITION_NAME),)
+#    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)
+#else
+#    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/config
+#endif
+#endif
 
 # whitelisted app
 PRODUCT_COPY_FILES += \
@@ -58,14 +58,14 @@ endif
 # Permission for Wi-Fi passpoint support
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
 
-PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
+#PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 PRODUCT_PACKAGES += qcril.db
 
-ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)
-ifneq ($(TARGET_HAS_LOW_RAM), true)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-endif
-endif
+#ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)
+#ifneq ($(TARGET_HAS_LOW_RAM), true)
+#$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+#endif
+#endif
 
 #$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 #$(call inherit-product, frameworks/base/data/keyboards/keyboards.mk)
