56c88afc09c8eb8547f85e091e92667409567c8c
klozz <carlosj@klozz.dev>
2022-07-21 11:29:18 +0200

    dont enable stuff for davinci
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: I69dab3bd3eae4eac30714d230c6a4916a5bcbeaf

--- build/tasks/vendor_bootjars.mk
+++ build/tasks/vendor_bootjars.mk
@@ -1,11 +1,12 @@
 # This makefile is used to include
 # extra product boot jars for SDK
-
+ifneq ($(TARGET_DEVICE),davinci)
 ifneq ($(VENDOR_QTI_PLATFORM),qssi)
 ifneq ($(call is-vendor-board-platform,QCOM),true)
 
 #call dex_preopt.mk for extra jars
 include $(BUILD_SYSTEM)/dex_preopt.mk
 
+endif
 endif
 endif
